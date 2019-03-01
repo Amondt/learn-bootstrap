a974aab9b4d0536d13f8313afc3c4eda7c5ab6e4
Amondt <mondt.alexande>
2019-03-01 14:11:01 +0100

    wip restaurant js page

--- restaurant-project/js/scripts.js
+++ restaurant-project/js/scripts.js
@@ -131,4 +131,498 @@ window.onload = () => {
         e.preventDefault() // prevent the default action (scroll / move caret)
     })
 
+    // Restaurants
+    const insideRestaurantsDiv = document.querySelector('#restaurants').firstElementChild.firstElementChild.childNodes[1]
+
+    document.querySelector('#brussels').addEventListener('click', brusselsDisplay)
+    document.querySelector('#paris').addEventListener('click', parisDisplay)
+    document.querySelector('#montreal').addEventListener('click', montrealDisplay)
+
+    // Header restaurant pages display
+    function imagesLinkHeader (container) {
+        const row = document.createElement('div')
+        row.setAttribute('class', 'row mb-5')
+
+        const col = document.createElement('div')
+        col.setAttribute('class', 'col')
+
+        const linkB = document.createElement('a')
+        linkB.id = "brussels"
+        linkB.href = "#"
+        const brussels = document.createElement('img')
+        brussels.setAttribute('class', 'mx-2')
+        brussels.src = "./img/brussels.jpg"
+        brussels.alt = "Brussels restaurant"
+        brussels.style.width = "70px"
+        brussels.style.height = "70px"
+
+        const linkP = document.createElement('a')
+        linkP.id = "paris"
+        linkP.href = "#"
+        const paris = document.createElement('img')
+        paris.setAttribute('class', 'mx-2')
+        paris.src = "./img/paris.jpg"
+        paris.alt = "Paris restaurant"
+        paris.style.width = "70px"
+        paris.style.height = "70px"
+
+        const linkM = document.createElement('a')
+        linkM.id = "montreal"
+        linkM.href = "#"
+        const montreal = document.createElement('img')
+        montreal.setAttribute('class', 'mx-2')
+        montreal.src = "./img/montreal.jpg"
+        montreal.alt = "Montreal restaurant"
+        montreal.style.width = "70px"
+        montreal.style.height = "70px"
+
+        linkB.appendChild(brussels)
+        linkP.appendChild(paris)
+        linkM.appendChild(montreal)
+
+        col.appendChild(linkB)
+        col.appendChild(linkP)
+        col.appendChild(linkM)
+
+        row.appendChild(col)
+
+        container.appendChild(row)
+
+        return container
+    }
+
+    // Brussels displays
+    function brusselsDisplay(e) {
+        e.preventDefault()
+        insideRestaurantsDiv.lastElementChild.remove()
+
+        const container = document.createElement('div')
+        container.setAttribute('class', 'container')
+
+        // Images links resto
+        imagesLinkHeader(container)
+
+        // PLace | Shifts
+        infosRestaurantBrussels(container)
+
+        // Google map api
+        mapBrussels(container)
+        
+        insideRestaurantsDiv.appendChild(container)
+        document.querySelector('#brussels').removeEventListener('click', brusselsDisplay)
+        document.querySelector('#paris').addEventListener('click', parisDisplay)
+        document.querySelector('#montreal').addEventListener('click', montrealDisplay)
+    }
+    function infosRestaurantBrussels(container) {
+        const row = document.createElement('div')
+        row.setAttribute('class', 'row')
+
+        // left col js init
+        const colLeft = document.createElement('div')
+        colLeft.setAttribute('class', 'col-6')
+
+        const rowLeft = document.createElement('div')
+        rowLeft.setAttribute('class', 'row')
+
+        const colLeftIcon = document.createElement('div')
+        colLeftIcon.setAttribute('class', 'col-2')
+
+        const iLocation = document.createElement('i')
+        iLocation.setAttribute('class', "fas fa-map-marker-alt text-center mt-1")
+        iLocation.style.fontSize = '2.5rem'
+
+        const colLeftLocation = document.createElement('div')
+        colLeftLocation.setAttribute('class', 'col-10')
+
+        const h3MM = document.createElement('h3')
+        h3MM.textContent = "Mamma Mia | Bruxelles"
+        h3MM.setAttribute('class', 'text-left')
+        const pAdress = document.createElement('p')
+        pAdress.textContent = "Place Sainte-Catherine 8, 1000 Bruxelles"
+        pAdress.setAttribute('class', 'text-left')
+
+        const colLeftPhoneI = document.createElement('div')
+        colLeftPhoneI.setAttribute('class', 'col-2')
+
+        const iPhone = document.createElement('i')
+        iPhone.setAttribute('class', "fas fa-phone text-center mt-1")
+        iPhone.style.fontSize = '2rem'
+
+        const colLeftPhone = document.createElement('div')
+        colLeftPhone.setAttribute('class', 'col-10')
+
+        const h3Phone = document.createElement('h3')
+        h3Phone.textContent = "+32 495 56 65 74"
+        h3Phone.setAttribute('class', 'text-left')
+
+        // right col js init
+        const colRight = document.createElement('div')
+        colRight.setAttribute('class', 'col-6')
+
+        const rowRight = document.createElement('div')
+        rowRight.setAttribute('class', 'row')
+
+        const colRightIcon = document.createElement('div')
+        colRightIcon.setAttribute('class', 'col-2')
+
+        const iShifts = document.createElement('i')
+        iShifts.setAttribute('class', "far fa-clock text-center mt-1")
+        iShifts.style.fontSize = '2.5rem'
+
+        const colRightShifts = document.createElement('div')
+        colRightShifts.setAttribute('class', 'col-10')
+
+        const h3Midi = document.createElement('h3')
+        h3Midi.textContent = "Midi"
+        h3Midi.setAttribute('class', 'text-left')
+        const pMidi = document.createElement('p')
+        pMidi.textContent = "lundi au vendredi, de 11h30 à 14h30"
+        pMidi.setAttribute('class', 'text-left')
+
+        const h3Soir = document.createElement('h3')
+        h3Soir.textContent = "Soir"
+        h3Soir.setAttribute('class', 'text-left')
+        const pSoir = document.createElement('p')
+        pSoir.textContent = "lundi à samedi, de 18h30 à 22h30"
+        pSoir.setAttribute('class', 'text-left')
+
+        // appends on Html
+        container.appendChild(row)
+
+        row.appendChild(colLeft)
+        row.appendChild(colRight)
+
+        // Left part appends | location - phone
+        colLeft.appendChild(rowLeft)
+        rowLeft.appendChild(colLeftIcon)
+        colLeftIcon.appendChild(iLocation)
+        rowLeft.appendChild(colLeftLocation)
+        colLeftLocation.appendChild(h3MM)
+        colLeftLocation.appendChild(pAdress)
+        rowLeft.appendChild(colLeftPhoneI)
+        colLeftPhoneI.appendChild(iPhone)
+        rowLeft.appendChild(colLeftPhone)
+        colLeftPhone.appendChild(h3Phone)
+
+        // Right part appends | shifts
+        colRight.appendChild(rowRight)
+        rowRight.appendChild(colRightIcon)
+        colRightIcon.appendChild(iShifts)
+        rowRight.appendChild(colRightShifts)
+        colRightShifts.appendChild(h3Midi)
+        colRightShifts.appendChild(pMidi)
+        colRightShifts.appendChild(h3Soir)
+        colRightShifts.appendChild(pSoir)
+
+        return container
+    }
+    function mapBrussels(container) {
+        const row = document.createElement('div')
+        row.setAttribute('class', 'row mt-5')
+
+        const col = document.createElement('div')
+        col.setAttribute('class', 'col')
+
+        const mapFrame = document.createElement('iframe')
+        mapFrame.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d629.7340750526905!2d4.34726882925014!3d50.85086384541537!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTDCsDUxJzAzLjEiTiA0wrAyMCc1Mi4xIkU!5e0!3m2!1sfr!2sbe!4v1551443866086"
+        mapFrame.id = "map"
+        mapFrame.width = "600"
+        mapFrame.frameborder = "0"
+
+        container.appendChild(row)
+        row.appendChild(col)
+        col.appendChild(mapFrame)
+
+        return container
+    }
+
+    // Paris displays
+    function parisDisplay(e) {
+        e.preventDefault()
+        insideRestaurantsDiv.lastElementChild.remove()
+
+        const container = document.createElement('div')
+        container.setAttribute('class', 'container')
+
+        // Images links resto
+        imagesLinkHeader(container)
+
+        // PLace | Shifts
+        infosRestaurantParis(container)
+
+        // Google map api
+        mapParis(container)
+        
+        insideRestaurantsDiv.appendChild(container)
+        document.querySelector('#paris').removeEventListener('click', parisDisplay)
+        document.querySelector('#brussels').addEventListener('click', brusselsDisplay)
+        document.querySelector('#montreal').addEventListener('click', montrealDisplay)
+    }
+    function infosRestaurantParis(container) {
+        const row = document.createElement('div')
+        row.setAttribute('class', 'row')
+
+        // left col js init
+        const colLeft = document.createElement('div')
+        colLeft.setAttribute('class', 'col-6')
+
+        const rowLeft = document.createElement('div')
+        rowLeft.setAttribute('class', 'row')
+
+        const colLeftIcon = document.createElement('div')
+        colLeftIcon.setAttribute('class', 'col-2')
+
+        const iLocation = document.createElement('i')
+        iLocation.setAttribute('class', "fas fa-map-marker-alt text-center mt-1")
+        iLocation.style.fontSize = '2.5rem'
+
+        const colLeftLocation = document.createElement('div')
+        colLeftLocation.setAttribute('class', 'col-10')
+
+        const h3MM = document.createElement('h3')
+        h3MM.textContent = "Mamma Mia | Paris"
+        h3MM.setAttribute('class', 'text-left')
+        const pAdress = document.createElement('p')
+        pAdress.textContent = "Place de la Contrescarpe 2, 75005 Paris"
+        pAdress.setAttribute('class', 'text-left')
+
+        const colLeftPhoneI = document.createElement('div')
+        colLeftPhoneI.setAttribute('class', 'col-2')
+
+        const iPhone = document.createElement('i')
+        iPhone.setAttribute('class', "fas fa-phone text-center mt-1")
+        iPhone.style.fontSize = '2rem'
+
+        const colLeftPhone = document.createElement('div')
+        colLeftPhone.setAttribute('class', 'col-10')
+
+        const h3Phone = document.createElement('h3')
+        h3Phone.textContent = "+33 1 23 45 67 89"
+        h3Phone.setAttribute('class', 'text-left')
+
+        // right col js init
+        const colRight = document.createElement('div')
+        colRight.setAttribute('class', 'col-6')
+
+        const rowRight = document.createElement('div')
+        rowRight.setAttribute('class', 'row')
+
+        const colRightIcon = document.createElement('div')
+        colRightIcon.setAttribute('class', 'col-2')
+
+        const iShifts = document.createElement('i')
+        iShifts.setAttribute('class', "far fa-clock text-center mt-1")
+        iShifts.style.fontSize = '2.5rem'
+
+        const colRightShifts = document.createElement('div')
+        colRightShifts.setAttribute('class', 'col-10')
+
+        const h3Midi = document.createElement('h3')
+        h3Midi.textContent = "Midi"
+        h3Midi.setAttribute('class', 'text-left')
+        const pMidi = document.createElement('p')
+        pMidi.textContent = "lundi au vendredi, de 11h30 à 14h30"
+        pMidi.setAttribute('class', 'text-left')
+
+        const h3Soir = document.createElement('h3')
+        h3Soir.textContent = "Soir"
+        h3Soir.setAttribute('class', 'text-left')
+        const pSoir = document.createElement('p')
+        pSoir.textContent = "lundi à samedi, de 18h30 à 22h30"
+        pSoir.setAttribute('class', 'text-left')
+
+        // appends on Html
+        container.appendChild(row)
+
+        row.appendChild(colLeft)
+        row.appendChild(colRight)
+
+        // Left part appends | location - phone
+        colLeft.appendChild(rowLeft)
+        rowLeft.appendChild(colLeftIcon)
+        colLeftIcon.appendChild(iLocation)
+        rowLeft.appendChild(colLeftLocation)
+        colLeftLocation.appendChild(h3MM)
+        colLeftLocation.appendChild(pAdress)
+        rowLeft.appendChild(colLeftPhoneI)
+        colLeftPhoneI.appendChild(iPhone)
+        rowLeft.appendChild(colLeftPhone)
+        colLeftPhone.appendChild(h3Phone)
+
+        // Right part appends | shifts
+        colRight.appendChild(rowRight)
+        rowRight.appendChild(colRightIcon)
+        colRightIcon.appendChild(iShifts)
+        rowRight.appendChild(colRightShifts)
+        colRightShifts.appendChild(h3Midi)
+        colRightShifts.appendChild(pMidi)
+        colRightShifts.appendChild(h3Soir)
+        colRightShifts.appendChild(pSoir)
+
+        return container
+    }
+    function mapBrussels(container) {
+        const row = document.createElement('div')
+        row.setAttribute('class', 'row mt-5')
+
+        const col = document.createElement('div')
+        col.setAttribute('class', 'col')
+
+        const mapFrame = document.createElement('iframe')
+        mapFrame.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d629.7340750526905!2d4.34726882925014!3d50.85086384541537!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTDCsDUxJzAzLjEiTiA0wrAyMCc1Mi4xIkU!5e0!3m2!1sfr!2sbe!4v1551443866086"
+        mapFrame.id = "map"
+        mapFrame.width = "600"
+        mapFrame.frameborder = "0"
+
+        container.appendChild(row)
+        row.appendChild(col)
+        col.appendChild(mapFrame)
+
+        return container
+    }
+
+    // Montreal displays
+    function brusselsDisplay(e) {
+        e.preventDefault()
+        insideRestaurantsDiv.lastElementChild.remove()
+
+        const container = document.createElement('div')
+        container.setAttribute('class', 'container')
+
+        // Images links resto
+        imagesLinkHeader(container)
+
+        // PLace | Shifts
+        infosRestaurantBrussels(container)
+
+        // Google map api
+        mapBrussels(container)
+        
+        insideRestaurantsDiv.appendChild(container)
+        document.querySelector('#montreal').removeEventListener('click', montrealDisplay)
+        document.querySelector('#brussels').addEventListener('click', brusselsDisplay)
+        document.querySelector('#paris').addEventListener('click', parisDisplay)
+    }
+    function infosRestaurantBrussels(container) {
+        const row = document.createElement('div')
+        row.setAttribute('class', 'row')
+
+        // left col js init
+        const colLeft = document.createElement('div')
+        colLeft.setAttribute('class', 'col-6')
+
+        const rowLeft = document.createElement('div')
+        rowLeft.setAttribute('class', 'row')
+
+        const colLeftIcon = document.createElement('div')
+        colLeftIcon.setAttribute('class', 'col-2')
+
+        const iLocation = document.createElement('i')
+        iLocation.setAttribute('class', "fas fa-map-marker-alt text-center mt-1")
+        iLocation.style.fontSize = '2.5rem'
+
+        const colLeftLocation = document.createElement('div')
+        colLeftLocation.setAttribute('class', 'col-10')
+
+        const h3MM = document.createElement('h3')
+        h3MM.textContent = "Mamma Mia | Bruxelles"
+        h3MM.setAttribute('class', 'text-left')
+        const pAdress = document.createElement('p')
+        pAdress.textContent = "Place Sainte-Catherine 8, 1000 Bruxelles"
+        pAdress.setAttribute('class', 'text-left')
+
+        const colLeftPhoneI = document.createElement('div')
+        colLeftPhoneI.setAttribute('class', 'col-2')
+
+        const iPhone = document.createElement('i')
+        iPhone.setAttribute('class', "fas fa-phone text-center mt-1")
+        iPhone.style.fontSize = '2rem'
+
+        const colLeftPhone = document.createElement('div')
+        colLeftPhone.setAttribute('class', 'col-10')
+
+        const h3Phone = document.createElement('h3')
+        h3Phone.textContent = "+32 495 56 65 74"
+        h3Phone.setAttribute('class', 'text-left')
+
+        // right col js init
+        const colRight = document.createElement('div')
+        colRight.setAttribute('class', 'col-6')
+
+        const rowRight = document.createElement('div')
+        rowRight.setAttribute('class', 'row')
+
+        const colRightIcon = document.createElement('div')
+        colRightIcon.setAttribute('class', 'col-2')
+
+        const iShifts = document.createElement('i')
+        iShifts.setAttribute('class', "far fa-clock text-center mt-1")
+        iShifts.style.fontSize = '2.5rem'
+
+        const colRightShifts = document.createElement('div')
+        colRightShifts.setAttribute('class', 'col-10')
+
+        const h3Midi = document.createElement('h3')
+        h3Midi.textContent = "Midi"
+        h3Midi.setAttribute('class', 'text-left')
+        const pMidi = document.createElement('p')
+        pMidi.textContent = "lundi au vendredi, de 11h30 à 14h30"
+        pMidi.setAttribute('class', 'text-left')
+
+        const h3Soir = document.createElement('h3')
+        h3Soir.textContent = "Soir"
+        h3Soir.setAttribute('class', 'text-left')
+        const pSoir = document.createElement('p')
+        pSoir.textContent = "lundi à samedi, de 18h30 à 22h30"
+        pSoir.setAttribute('class', 'text-left')
+
+        // appends on Html
+        container.appendChild(row)
+
+        row.appendChild(colLeft)
+        row.appendChild(colRight)
+
+        // Left part appends | location - phone
+        colLeft.appendChild(rowLeft)
+        rowLeft.appendChild(colLeftIcon)
+        colLeftIcon.appendChild(iLocation)
+        rowLeft.appendChild(colLeftLocation)
+        colLeftLocation.appendChild(h3MM)
+        colLeftLocation.appendChild(pAdress)
+        rowLeft.appendChild(colLeftPhoneI)
+        colLeftPhoneI.appendChild(iPhone)
+        rowLeft.appendChild(colLeftPhone)
+        colLeftPhone.appendChild(h3Phone)
+
+        // Right part appends | shifts
+        colRight.appendChild(rowRight)
+        rowRight.appendChild(colRightIcon)
+        colRightIcon.appendChild(iShifts)
+        rowRight.appendChild(colRightShifts)
+        colRightShifts.appendChild(h3Midi)
+        colRightShifts.appendChild(pMidi)
+        colRightShifts.appendChild(h3Soir)
+        colRightShifts.appendChild(pSoir)
+
+        return container
+    }
+    function mapBrussels(container) {
+        const row = document.createElement('div')
+        row.setAttribute('class', 'row mt-5')
+
+        const col = document.createElement('div')
+        col.setAttribute('class', 'col')
+
+        const mapFrame = document.createElement('iframe')
+        mapFrame.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d629.7340750526905!2d4.34726882925014!3d50.85086384541537!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTDCsDUxJzAzLjEiTiA0wrAyMCc1Mi4xIkU!5e0!3m2!1sfr!2sbe!4v1551443866086"
+        mapFrame.id = "map"
+        mapFrame.width = "600"
+        mapFrame.frameborder = "0"
+
+        container.appendChild(row)
+        row.appendChild(col)
+        col.appendChild(mapFrame)
+
+        return container
+    }
 }
